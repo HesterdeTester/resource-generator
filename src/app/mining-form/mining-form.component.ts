f1f97a6e42c846c4b6dc438c9d57aa18c7875c27
HesterdeTester <37218899+HesterdeTester@users.noreply.github.com>
2019-08-04 09:30:22 +0200

    added json with geodes

--- src/app/mining-form/mining-form.component.ts
+++ src/app/mining-form/mining-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MiningCalcService } from '../services/mining-calc.service';
+import Geodes from '../../assets/data/geodes.json';
 
 @Component({
   selector: 'app-mining-form',
@@ -10,18 +11,20 @@ import { MiningCalcService } from '../services/mining-calc.service';
 export class MiningFormComponent implements OnInit {
   miningForm: FormGroup;
   message: string;
+  geodes: any = Geodes;
 
   constructor(private miningCalcService: MiningCalcService) { }
 
   ngOnInit() {
+    console.log(this.geodes);
     this.miningForm = new FormGroup({
-      planet: new FormControl(1, [Validators.required]),
+      geode: new FormControl(1, [Validators.required]),
       level: new FormControl(1, [Validators.required]),
     });
   }
   onSubmit() {
     console.log(this.miningForm.value);
-    this.message = JSON.stringify(this.miningCalcService.getResult());
+    this.message = JSON.stringify(this.miningCalcService.getResult(1, 2)); // moet andere waardes (level,geode)
   }
 
 }
--- src/app/services/mining-calc.service.ts
+++ src/app/services/mining-calc.service.ts
@@ -12,52 +12,54 @@ export class MiningCalcService {
 
   constructor() { }
 
-  getResult(): any {
-    return this.mockResult;
+  getResult(geode, level): any {
+    const chances = this.getChances(geode);
+    return this.calculator(level, chances);
   }
-  getChances(planet) {
-    // krijg de unubtaniumChance,	suriliumChance,	daliumChance,	blarniumChance
+
+  getChances(geode) {
+    const chanceRest = 21;
+    return chanceRest;
+    // krijg de unubtaniumChance,	suriliumChance,	daliumChance,	blarniumChance, of endrange, in een json
   }
 
   calculator(level = 10, unubtaniumChance = 21, suriliumChance = 47, daliumChance = 9, blarniumChance = 21): any {  // nu nog hardcoded
-    const loops = level + getRandomInt((level / 2), level);
-    const unubtaniumEndRange = unubtaniumChance - 1;
+    const loops = level + this.getRandomInt((level / 2), level);
+    const unubtaniumEndRange = unubtaniumChance - 1; // chance.unubtanium (want JSON), zou ook endRange terug kunnen krijgen
     const suriliumEndRange = unubtaniumChance + suriliumChance - 1;
     const daliumEndRange = unubtaniumChance + suriliumChance + daliumChance - 1;
     const blarniumEndRange = unubtaniumChance + suriliumChance + daliumChance + blarniumChance - 1;
 
-    const grondstoffen = getGrondstof(level, unubtaniumEndRange, suriliumEndRange, daliumEndRange, blarniumEndRange);
-
-    function getRandomInt(min, max) {
-      return Math.floor(Math.random() * max) + min;
-    }
-
-    function inRange(x, min, max) {
-      return ((x - min) * (x - max) <= 0);
-    }
-
-    function getGrondstof(loops, unubtaniumEndRange, suriliumEndRange, daliumEndRange, blarniumEndRange) {
-      const result = [];
-      for (let i = 0; i < loops; i++) {
-        const calcChance: number = getRandomInt(0, 99); // 99 nodig, anders krijgt empty 3 procent kans ipv 2
-        if (inRange(calcChance, 0, unubtaniumEndRange)) {
-          result.push('Unubtanium');
-        }
-        if (inRange(calcChance, (unubtaniumEndRange + 1) , suriliumEndRange)) {
-          result.push('Surilium');
-        }
-        if (inRange(calcChance, (suriliumEndRange + 1), daliumEndRange)) {
-          result.push('Dalium');
-        }
-        if (inRange(calcChance, (daliumEndRange + 1), blarniumEndRange)) {
-          result.push('Blarnium');
-        } else {
-          result.push('Empty');   // misschien raar als je level 10 bent en je 10 grondstoffen verwacht,
-                                  // dan krijg je er 7 waarvan 3 'empty', over nadenken
-        }
-        return result;
+    const grondstoffen = this.getGrondstof(loops, unubtaniumEndRange, suriliumEndRange, daliumEndRange, blarniumEndRange);
+    return grondstoffen;
+  }
+  getGrondstof(loops, unubtaniumEndRange, suriliumEndRange, daliumEndRange, blarniumEndRange) {
+    const result = [];
+    for (let i = 0; i < loops; i++) {
+      const calcChance: number = this.getRandomInt(0, 99); // 99 nodig, anders krijgt empty 3 procent kans ipv 2
+      if (this.inRange(calcChance, 0, unubtaniumEndRange)) {
+        result.push('Unubtanium');
       }
-
-      return grondstoffen;
+      if (this.inRange(calcChance, (unubtaniumEndRange + 1), suriliumEndRange)) {
+        result.push('Surilium');
+      }
+      if (this.inRange(calcChance, (suriliumEndRange + 1), daliumEndRange)) {
+        result.push('Dalium');
+      }
+      if (this.inRange(calcChance, (daliumEndRange + 1), blarniumEndRange)) {
+        result.push('Blarnium');
+      } else {
+        result.push('Empty');   // misschien raar als je level 10 bent en je 10 grondstoffen verwacht,
+        // dan krijg je er 7 waarvan 3 'empty', over nadenken
+      }
+      return result;
     }
   }
+
+  getRandomInt(min, max) {
+    return Math.floor(Math.random() * max) + min;
+  }
+  inRange(x, min, max) {
+    return ((x - min) * (x - max) <= 0);
+  }
+}
