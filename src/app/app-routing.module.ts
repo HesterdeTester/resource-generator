bb6f29f03ed672ef2b077bac358d4f70d4f6055b
HesterdeTester <37218899+HesterdeTester@users.noreply.github.com>
2019-08-04 22:06:29 +0200

    added service worker, so we can work offline

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MiningFormComponent } from './mining-form/mining-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MiningFormComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MiningFormComponent } from './mining-form/mining-form.component';
 import { MiningChartComponent } from './mining-form/mining-chart/mining-chart.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,8 @@ import { MiningChartComponent } from './mining-form/mining-chart/mining-chart.co
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
